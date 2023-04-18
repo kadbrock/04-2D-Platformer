f5ef081a995fc4414ef41e0f3dfe7ba58a1b8e1e
BL-MSCH-C220-F22 <121530054+BL-MSCH-C220@users.noreply.github.com>
2023-04-18 07:17:45 -0400

    Just connected up the script
    You hadn't actually attached the script to the enemy. It seems to be working now. Looks good so far!

--- Enemy/Enemy_Ground.gd
+++ Enemy/Enemy_Ground.gd
@@ -1,17 +1,13 @@
 extends KinematicBody2D
 
-export var speed = 2
+export var speed = 20
 export var damage = 10
 var direction = 1
 var velocity = Vector2.ZERO
 
 
 func _physics_process(_delta):
-	print("Physics process called")
 	velocity.x = direction * speed
-	print("Velocity: ", velocity)
 	velocity = move_and_slide(velocity, Vector2.UP)
 	if is_on_wall():
-		print("Collision detected")
 		direction *= -1
-	print("Position: ", position)
